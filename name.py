4e2d01941de06717fe207416a785f9a24f931d3d
Yifen123123 <151841897+Yifen123123@users.noreply.github.com>
2025-09-22 16:53:14 +0800

    Add files via upload

--- name.py
+++ name.py
@@ -1,12 +1,15 @@
 pat = re.compile(get_name_pattern(get_name_list()))
 
 for m in pat.finditer(text):
+    # 沒有身分證 → 跳過
+    if not m.group('twid'):
+        continue
+
     parts = [f"主名：{m.group('name')}"]
 
-    if m.group('alias'):   # 只有有別名才加
+    if m.group('alias'):   # 有別名才加
         parts.append(f"原名：{m.group('alias')}")
 
-    if m.group('twid'):    # 只有有身分證才加
-        parts.append(f"身分證：{m.group('twid')}")
+    parts.append(f"身分證：{m.group('twid')}")
 
     print("；".join(parts))
